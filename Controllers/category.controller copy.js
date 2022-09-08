7ae6bc01a475c6bfc9d83239617af11f3c010303
Raygud <runigudmundarson@hotmail.com>
2022-09-08 14:12:08 +0200

    Category controller added

---
+++ Controllers/category.controller copy.js
@@ -0,0 +1,79 @@
+import {Sequelize} from 'sequelize'
+import CategoryModel from "../Models/Category.model.js"
+
+
+
+
+class CategoryController {
+constructor(){
+    console.log("Instance call of User controller")
+}
+
+    list = async (req, res) => {
+        const result = await CategoryModel.findAll()
+        res.json(result)
+    }
+
+    get = async (req, res) => {
+        const result = await CategoryModel.findOne({
+            where: { id: req.params.id },
+        })
+        res.json(result);
+    }
+
+    create = async (req,res) =>{
+        const { title } = req.body;
+
+        if( title ){
+            const model = await CategoryModel.create(req.body);
+            return res.json({newId: model.id});
+        }else{
+            res.send(418);
+        }
+
+    }
+
+    update = async (req,res) =>{
+        const { title } = req.body;
+
+        if(title){
+            const model = await CategoryModel.update(req.body,{
+                where: { id: req.params.id },
+                individualHooks: true
+            });
+            return res.json({status: true});
+        }else{
+            res.send(418);
+        }
+
+    }
+
+    delete = async (req, res) => {
+
+        try {
+
+            await CategoryModel.destroy({
+
+                where: {
+
+                    id: req.params.id
+
+                }
+
+            })
+
+            res.sendStatus(200)
+
+        } catch (error) {
+
+            res.send(error)
+
+           
+
+        }
+
+    }
+
+}
+
+export {CategoryController}
