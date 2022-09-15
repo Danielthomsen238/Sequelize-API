7aec1033a92ae4e0d7977a40653d78358f7f4538
Optic-Dex <danielthomsen238@gmail.com>
2022-09-15 14:22:37 +0200

    Hola

--- Routes/user.router.js
+++ Routes/user.router.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { UserController } from '../Controllers/User.controller.js'
+import verifyToken from '../Middleware/verifyToken.js'
 
 const controller = new UserController();
 
 const router = express.Router()
 
-router.get('/User', (req, res) => {controller.list(req,res)})
+router.get('/User',(req, res) => {controller.list(req,res)})
 router.get('/User/:id[0-9]*', (req, res) => {controller.get(req,res)})
 router.post('/User', (req, res) => {controller.create(req,res)})
 router.put('/User', (req, res) => {controller.update(req,res)})
