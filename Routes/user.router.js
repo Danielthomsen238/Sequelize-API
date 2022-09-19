4e91726f609ac54e78c45b347e5a59b4efa4b1c8
Optic-Dex <danielthomsen238@gmail.com>
2022-09-19 13:09:19 +0200

    update user

--- Routes/user.router.js
+++ Routes/user.router.js
@@ -9,7 +9,7 @@ const UserRouter = express.Router()
 UserRouter.get('/User', verifyToken, (req, res) => {controller.list(req,res)})
 UserRouter.get('/User/:id[0-9]*', verifyToken, (req, res) => {controller.get(req,res)})
 UserRouter.post('/User', (req, res) => {controller.create(req,res)})
-UserRouter.put('/User', (req, res) => {controller.update(req,res)})
+UserRouter.put('/User', verifyToken, (req, res) => {controller.update(req,res)})
 UserRouter.delete('/User/:id([0-9]*)', verifyToken, (req, res) => {
     controller.delete(req,res)
  })
