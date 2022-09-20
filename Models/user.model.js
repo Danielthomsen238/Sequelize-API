c540fce0c1859d6d64a504ee1ea61a523cfb977d
Optic-Dex <danielthomsen238@gmail.com>
2022-09-20 13:33:06 +0200

    asd

--- Models/user.model.js
+++ Models/user.model.js
@@ -1,78 +1,80 @@
-const { sequelize } = require('../Config/db.sequelize.js') 
-const {DataTypes} = require('sequelize')
-const {Model} = require('sequelize')
-
+const { sequelize } = require("../Config/db.sequelize.js");
+const { DataTypes } = require("sequelize");
+const { Model } = require("sequelize");
 
 class UserModel extends Model {}
 
-UserModel.init({
+UserModel.init(
+  {
     id: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        allowNull: false,
-        primaryKey: true
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
     },
     firstname: {
-        type: DataTypes.CHAR,
-        allowNull: false
+      type: DataTypes.CHAR,
+      allowNull: false,
     },
     lastname: {
-        type: DataTypes.CHAR,
-        allowNull: false
+      type: DataTypes.CHAR,
+      allowNull: false,
     },
     telefon: {
-        type: DataTypes.INTEGER,
-        allowNull: false
+      type: DataTypes.INTEGER,
+      allowNull: false,
     },
     email: {
-        type: DataTypes.CHAR,
-        allowNull: false
+      type: DataTypes.CHAR,
+      allowNull: false,
     },
     password: {
-        type: DataTypes.CHAR,
-        allowNull: false
+      type: DataTypes.CHAR,
+      allowNull: false,
     },
     active: {
-        type: DataTypes.BOOLEAN,
-        allowNull: false,
-        defaultValue: false
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false,
     },
     school_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
+      type: DataTypes.INTEGER,
+      allowNull: false,
     },
     role_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        defaultValue: 2
-    }
-},{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 2,
+    },
+  },
+  {
     sequelize,
-    modelName: 'user',
+    modelName: "user",
     freezeTableName: true,
-    underscored: true ,
+    underscored: true,
     createdAt: true,
     updatedAt: true,
     hooks: {
-        beforeCreate: async (user, options) => {
-            user.password = await createHash(user.password)
-        },
-        // beforeUpdate: async (user, options) => {
-        //     user.password = await createHash(user.password)
-        // }
-    }
-})
+      beforeCreate: async (user, options) => {
+        user.password = await createHash(user.password);
+      },
+      // beforeUpdate: async (user, options) => {
+      //     user.password = await createHash(user.password)
+      // }
+    },
+  }
+);
 
 /**
  * Funktion that encrypts a string
- * @param {String} string 
+ * @param {String} string
  * @returns Hashed string
  */
 
-const createHash = async string => {
-    const salt = await bcrypt.genSalt(10);
-    const hashedString = await bcrypt.hash(string, salt);
-    return hashedString;
-}
+const createHash = async (string) => {
+  const salt = await bcrypt.genSalt(10);
+  const hashedString = await bcrypt.hash(string, salt);
+  return hashedString;
+};
 
-module.exports = UserModel
+module.exports = UserModel;
