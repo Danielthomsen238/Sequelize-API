5daf331c32292571a9b5a851ef42859ca350f3bd
Optic-Dex <danielthomsen238@gmail.com>
2022-09-26 11:36:32 +0200

    secure false

--- Models/user.model.js
+++ Models/user.model.js
@@ -108,7 +108,7 @@ const sendEmail = async (user_email, password) => {
   var transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: process.env.SMTP_PORT,
-    secure: true,
+    secure: false,
     auth: {
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASSWORD,
