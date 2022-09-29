d096e4bf3046db315617a38e76bad7abb33802a7
Optic-Dex <danielthomsen238@gmail.com>
2022-09-29 14:07:27 +0200

    sync

--- Controllers/course.controller.js
+++ Controllers/course.controller.js
@@ -36,9 +36,9 @@ class CourseController {
   };
 
   create = async (req, res) => {
-    const { name, description, duration, school_id, category_id } = req.body;
+    const { name, description, duration, category_id } = req.body;
 
-    if (name && description && duration && school_id && category_id) {
+    if (name && description && duration && category_id) {
       const model = await CourseModel.create(req.body);
       return res.json({ newId: model.id });
     } else {
