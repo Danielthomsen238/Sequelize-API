bd1110ab7b2b2f710ea692eef9490371a2b3ea56
Optic-Dex <danielthomsen238@gmail.com>
2022-09-29 10:01:50 +0200

    make it better

--- Controllers/course.controller.js
+++ Controllers/course.controller.js
@@ -1,83 +1,83 @@
-const CourseModel = require("../Models/course.model.js")
-const SchoolModel = require("../Models/school.model.js")
-const CategoryModel = require("../Models/category.model.js")
+const CourseModel = require("../Models/course.model.js");
+const SchoolModel = require("../Models/school.model.js");
+const CategoryModel = require("../Models/category.model.js");
 
-SchoolModel.hasMany(CourseModel)
-CourseModel.belongsTo(SchoolModel)
-CategoryModel.hasMany(CourseModel)
-CourseModel.belongsTo(CategoryModel)
-
+SchoolModel.hasMany(CourseModel);
+CourseModel.belongsTo(SchoolModel);
+CategoryModel.hasMany(CourseModel);
+CourseModel.belongsTo(CategoryModel);
 
 class CourseController {
-constructor(){
-    console.log("Instance call of Course controller")
+  constructor() {
+    console.log("Instance call of Course controller");
+  }
+
+  list = async (req, res) => {
+    const result = await CourseModel.findAll();
+    res.json(result);
+  };
+
+  get = async (req, res) => {
+    const result = await CourseModel.findOne({
+      where: { id: req.params.id },
+    });
+    res.json(result);
+  };
+
+  create = async (req, res) => {
+    const { name, description, duration, school_id, user_id, category_id } =
+      req.body;
+
+    if (
+      name &&
+      description &&
+      duration &&
+      school_id &&
+      user_id &&
+      category_id
+    ) {
+      const model = await CourseModel.create(req.body);
+      return res.json({ newId: model.id });
+    } else {
+      res.send(418);
+    }
+  };
+  //
+  update = async (req, res) => {
+    const { name, description, duration, school_id, user_id, category_id } =
+      req.body;
+
+    if (
+      name &&
+      description &&
+      duration &&
+      school_id &&
+      user_id &&
+      category_id
+    ) {
+      const model = await CourseModel.update(req.body, {
+        where: { id: req.params.id },
+        individualHooks: true,
+      });
+      return res.json({ status: true });
+    } else {
+      res.send(418);
+    }
+  };
+
+  delete = async (req, res) => {
+    try {
+      await CourseModel.destroy({
+        where: {
+          id: req.params.id,
+        },
+      });
+
+      res.sendStatus(200);
+    } catch (error) {
+      res.send(error);
+    }
+  };
 }
 
-    list = async (req, res) => {
-        const result = await CourseModel.findAll()
-        res.json(result)
-    }
-
-    get = async (req, res) => {
-        const result = await CourseModel.findOne({
-            where: { id: req.params.id },
-        })
-        res.json(result);
-    }
-
-    create = async (req,res) =>{
-        const { name, description, duration, school_id, user_id, category_id } = req.body;
-
-        if( name && description && duration && school_id && user_id && category_id ){
-            const model = await CourseModel.create(req.body);
-            return res.json({newId: model.id});
-        }else{
-            res.send(418);
-        }
-
-    }
-
-    update = async (req,res) =>{
-        const { name, description, duration, school_id, user_id, category_id } = req.body;
-
-        if( name && description && duration && school_id && user_id && category_id ){
-            const model = await CourseModel.update(req.body,{
-                where: { id: req.params.id },
-                individualHooks: true
-            });
-            return res.json({status: true});
-        }else{
-            res.send(418);
-        }
-
-    }
-
-    delete = async (req, res) => {
-
-        try {
-
-            await CourseModel.destroy({
-
-                where: {
-
-                    id: req.params.id
-
-                }
-
-            })
-
-            res.sendStatus(200)
-
-        } catch (error) {
-
-            res.send(error)
-
-           
-
-        }
-
-    }
-
-}
-
-module.exports = {CourseController}
+module.exports = { CourseController };
