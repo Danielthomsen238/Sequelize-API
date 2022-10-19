7ab65ace97ef4d727761f58cae346d8091c18d76
Optic-Dex <danielthomsen238@gmail.com>
2022-10-19 16:57:58 +0200

    sync

--- Models/course.model.js
+++ Models/course.model.js
@@ -1,44 +1,66 @@
-const { sequelize } = require('../Config/db.sequelize.js') 
-const {DataTypes} = require('sequelize')
-const {Model} = require('sequelize')
-
+const { sequelize } = require("../Config/db.sequelize.js");
+const { DataTypes } = require("sequelize");
+const { Model } = require("sequelize");
 
 class CoursesModel extends Model {}
 
-CoursesModel.init({
+CoursesModel.init(
+  {
     id: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        allowNull: false,
-        primaryKey: true
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
     },
     name: {
-        type: DataTypes.CHAR,
-        allowNull: false
+      type: DataTypes.CHAR,
+      allowNull: false,
     },
     description: {
-        type: DataTypes.CHAR,
-        allowNull: false
+      type: DataTypes.CHAR,
+      allowNull: false,
     },
     duration: {
-        type: DataTypes.CHAR,
-        allowNull: false
+      type: DataTypes.CHAR,
+      allowNull: false,
+    },
+    address: {
+      type: DataTypes.CHAR,
+      allowNull: false,
+    },
+    zip: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    city: {
+      type: DataTypes.CHAR,
+      allowNull: false,
+    },
+    lat: {
+      type: DataTypes.DOUBLE,
+      allowNull: false,
+    },
+    lng: {
+      type: DataTypes.DOUBLE,
+      allowNull: false,
     },
     school_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false
+      type: DataTypes.INTEGER,
+      allowNull: false,
     },
     category_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false
-    }
-},{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  },
+  {
     sequelize,
-    modelName: 'course',
+    modelName: "course",
     freezeTableName: true,
-    underscored: true ,
+    underscored: true,
     createdAt: true,
-    updatedAt: true
-})
+    updatedAt: true,
+  }
+);
 
-module.exports = CoursesModel
+module.exports = CoursesModel;
