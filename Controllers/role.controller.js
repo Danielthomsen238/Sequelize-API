ac436b607ce23000791e8038eb36642397ae3940
Optic-Dex <danielthomsen238@gmail.com>
2022-11-14 08:49:31 +0100

    sync

--- Controllers/role.controller.js
+++ Controllers/role.controller.js
@@ -1,79 +1,60 @@
-const RoleModel = require("../Models/role.model.js")
-
-
-
-
+const RoleModel = require("../Models/role.model.js");
 
 class RoleController {
-constructor(){
-    console.log("Instance call of role controller")
+  constructor() {
+    console.log("Instance call of role controller");
+  }
+
+  list = async (req, res) => {
+    const result = await RoleModel.findAll();
+    res.json(result);
+  };
+
+  get = async (req, res) => {
+    const result = await RoleModel.findOne({
+      where: { id: req.params.id },
+    });
+    res.json(result);
+  };
+  //
+  create = async (req, res) => {
+    const { role } = req.body;
+
+    if (role) {
+      const model = await RoleModel.create(req.body);
+      return res.json({ newId: model.id });
+    } else {
+      res.send(418);
+    }
+  };
+
+  update = async (req, res) => {
+    const { role } = req.body;
+
+    if (role) {
+      const model = await RoleModel.update(req.body, {
+        where: { id: req.params.id },
+        individualHooks: true,
+      });
+      return res.json({ status: true });
+    } else {
+      res.send(418);
+    }
+  };
+
+  delete = async (req, res) => {
+    try {
+      await RoleModel.destroy({
+        where: {
+          id: req.params.id,
+        },
+      });
+
+      res.sendStatus(200);
+    } catch (error) {
+      res.send(error);
+    }
+  };
 }
 
-    list = async (req, res) => {
-        const result = await RoleModel.findAll()
-        res.json(result)
-    }
-
-    get = async (req, res) => {
-        const result = await RoleModel.findOne({
-            where: { id: req.params.id },
-        })
-        res.json(result);
-    }
-
-    create = async (req,res) =>{
-        const { role } = req.body;
-
-        if( role ){
-            const model = await RoleModel.create(req.body);
-            return res.json({newId: model.id});
-        }else{
-            res.send(418);
-        }
-
-    }
-
-    update = async (req,res) =>{
-        const { role } = req.body;
-
-        if(role){
-            const model = await RoleModel.update(req.body,{
-                where: { id: req.params.id },
-                individualHooks: true
-            });
-            return res.json({status: true});
-        }else{
-            res.send(418);
-        }
-
-    }
-
-    delete = async (req, res) => {
-
-        try {
-
-            await RoleModel.destroy({
-
-                where: {
-
-                    id: req.params.id
-
-                }
-
-            })
-
-            res.sendStatus(200)
-
-        } catch (error) {
-
-            res.send(error)
-
-           
-
-        }
-
-    }
-
-}
-
-module.exports = {RoleController}
+module.exports = { RoleController };
